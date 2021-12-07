134eb9c9d2e842fac4e1063f59a6f2122932fb7d
Jens Streck <jens.streck@gmail.com>
2021-12-06 22:44:39 -0600

    part 1 of day 4 completej

--- 2021/04/src/bingo.rs
+++ 2021/04/src/bingo.rs
@@ -1,15 +1,159 @@
-pub struct Bingo_Tile {
-  value: u32,
-  is_marked: bool,
+use std::fs::File;
+use std::io::Result;
+use std::io::{BufRead, BufReader};
+
+const BOARD_SIZE: usize = 5;
+
+#[derive(Copy, Clone)]
+pub struct BingoTile {
+  pub value: u32,
+  pub is_marked: bool,
 }
 
-pub struct Bingo_Board {
-  layout: [[Bingo_Tile; 5];5],
+#[derive(Copy, Clone)]
+pub struct BingoBoard {
+  pub layout: [[BingoTile; BOARD_SIZE]; BOARD_SIZE],
 }
 
-impl Bingo_Board {
-
-  pub fn print_board(&self) {
-    println!("BINGO BOARD PRINTED")
+impl BingoBoard {
+  pub fn new() -> BingoBoard {
+    let &tile = &BingoTile {
+      value: 0,
+      is_marked: false,
+    };
+    let board = BingoBoard {
+      layout: [[tile; BOARD_SIZE]; BOARD_SIZE],
+    };
+    return board;
   }
+
+  pub fn print(&self) {
+    for y in self.layout.iter() {
+      for (i, x) in y.iter().enumerate() {
+        if x.is_marked {
+          print!("[{}]", x.value);
+        } else {
+          print!("{}", x.value);
+        }
+        if i != y.len() {
+          print!("\t");
+        }
+      }
+      print!("\n")
+    }
+    print!("\n");
+  }
+
+  pub fn call(&mut self, val: u32) -> bool {
+    'outer: for (i, row) in self.layout.iter().enumerate() {
+      for (j, tile) in row.iter().enumerate() {
+        if tile.value == val {
+          self.layout[i][j].is_marked = true;
+          break 'outer;
+        }
+      }
+    }
+
+    return self.check_win();
+  }
+
+  pub fn check_win(&self) -> bool {
+    // horizontal win
+    for row in self.layout {
+      let mut is_win = true;
+
+      for tile in row {
+        is_win = is_win && tile.is_marked;
+      }
+
+      if is_win {
+        return true;
+      }
+    }
+
+    // vertical win
+    for (i, _) in self.layout.iter().enumerate() {
+      let mut is_win = true;
+
+      for tile in self.layout {
+        is_win = is_win && tile[i].is_marked;
+      }
+
+      if is_win {
+        return true;
+      }
+    }
+
+    // diagonal win left
+    let mut is_win = true;
+    for (i, row) in self.layout.iter().enumerate() {
+      is_win = is_win && row[i].is_marked;
+    }
+    if is_win {
+      return true;
+    }
+
+    // diagonal win right
+    let mut is_win = true;
+    for (i, row) in self.layout.iter().enumerate() {
+      is_win = is_win && row[row.len() - 1 - i].is_marked;
+    }
+    if is_win {
+      return true;
+    }
+
+    return false;
+  }
+
+  pub fn add_unmarked(&self) -> u32 {
+    let mut num: u32 = 0;
+
+    for row in self.layout {
+      for tile in row {
+          if !tile.is_marked {
+            num += tile.value;
+          }
+      }
+    }
+
+    return num;
+  }
+}
+
+pub fn read_bingo_game_from_file(file_name: &str) -> Result<(Vec<u32>, Vec<BingoBoard>)> {
+  let file = File::open(file_name)?;
+  let reader = BufReader::new(file);
+
+  let mut lines = reader.lines();
+
+  let first_line = lines.next().unwrap()?;
+
+  let calls: Vec<u32> = first_line
+    .split(",")
+    .into_iter()
+    .map(|x| x.parse::<u32>().unwrap())
+    .collect(); // grab numbers from line and convert to i32
+
+  let mut boards: Vec<BingoBoard> = vec![];
+
+  let mut i = 0;
+  for line in lines {
+    let line = line.unwrap();
+    let row: Vec<&str> = line.trim().split_whitespace().collect();
+    if row.len() != BOARD_SIZE {
+      boards.push(BingoBoard::new());
+    } else {
+      let row: Vec<u32> = row.iter().map(|x| x.parse::<u32>().unwrap()).collect(); // grab numbers from line and convert to i32
+
+      // insert fields into board
+      for (j, x) in row.iter().enumerate() {
+        let r = i % BOARD_SIZE;
+
+        boards.last_mut().unwrap().layout[r][j].value = *x;
+      }
+      i += 1;
+    }
+  }
+
+  return Ok((calls, boards));
 }
--- 2021/04/src/main.rs
+++ 2021/04/src/main.rs
@@ -1,8 +1,6 @@
 mod bingo;
 
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use bingo::{Bingo_Board, Bingo_Tile};
+use bingo::BingoBoard;
 
 const FILE_PATH: &str = "resources/input.txt";
 
@@ -15,16 +13,32 @@ fn process_line(arr: &mut Vec<i32>, line: String) {
     }
 }
 
-fn part1(reader: BufReader<File>) {
-    let b: Bingo_Board;
-    b.print_board();
-}
+fn play_bingo(game: Vec<u32>, boards: &mut Vec<BingoBoard>) -> Result<(BingoBoard, u32), ()> {
+    for call in game {
+        for board in boards.iter_mut() {
+            let win: bool = board.call(call);
 
+            if win {
+                println!("Win Found");
+
+                return Ok((board.clone(), call));
+            }
+        }
+    }
+
+    Err(())
+}
 
 fn main() {
     println!("File: {}", FILE_PATH);
-    let file = File::open(FILE_PATH).unwrap();
-    let reader = BufReader::new(file);
+    let (game, mut boards) = bingo::read_bingo_game_from_file(FILE_PATH).unwrap();
 
-    part1(reader);
+    let winner_board = play_bingo(game, &mut boards);
+    let (winner_board, winning_number) = winner_board.unwrap();
+
+    winner_board.print();
+
+    let x = winner_board.add_unmarked();
+
+    println!("Value: {}", x * winning_number);
 }
