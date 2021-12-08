75fb6b427fa74f6a01d05656be9310d51027083a
Jens Streck <jens.streck@gmail.com>
2021-12-08 13:02:49 -0600

    part2 of day4 wip

--- 2021/04/src/main.rs
+++ 2021/04/src/main.rs
@@ -29,7 +29,32 @@ fn play_bingo(game: Vec<u32>, boards: &mut Vec<BingoBoard>) -> Result<(BingoBoar
     Err(())
 }
 
-fn main() {
+fn play_bingo_and_find_last_winner(game: Vec<u32>, boards: &Vec<BingoBoard>) -> (BingoBoard, u32) {
+
+    let mut recent_winner_board: BingoBoard;
+    let mut recent_winner_number: u32;
+    for call in game {
+        boards = boards.iter().filter_map(|&board| {
+
+            let win: bool = board.call(call);
+
+            if win {
+                println!("Win Found");
+
+                recent_winner_board = board.clone();
+                recent_winner_number = call;
+
+                return Some(board);
+            }
+
+            return None;
+        }).collect::<Vec<BingoBoard>>();
+    }
+
+    return (recent_winner_board, recent_winner_number);
+}
+
+fn part1() {
     println!("File: {}", FILE_PATH);
     let (game, mut boards) = bingo::read_bingo_game_from_file(FILE_PATH).unwrap();
 
@@ -41,4 +66,22 @@ fn main() {
     let x = winner_board.add_unmarked();
 
     println!("Value: {}", x * winning_number);
+}
+
+fn part2() {
+    println!("File: {}", FILE_PATH);
+    let (game, mut boards) = bingo::read_bingo_game_from_file(FILE_PATH).unwrap();
+
+    let (last_winner_board, winning_number)  = play_bingo_and_find_last_winner(game, &boards);
+
+    last_winner_board.print();
+
+    let x = last_winner_board.add_unmarked();
+
+    println!("Value: {}", x * winning_number);
+}
+
+fn main() {
+    // part1();
+    part2();
 }
