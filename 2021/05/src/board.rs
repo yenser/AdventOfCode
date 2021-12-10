5bb81409de564ba791dae3f78c81ecff997ce366
Jens Streck <jens.streck@gmail.com>
2021-12-09 20:37:49 -0600

    progress on day 5 part 1

--- 2021/05/src/board.rs
+++ 2021/05/src/board.rs
@@ -3,11 +3,11 @@ use std::io::Result;
 use std::io::{BufRead, BufReader};
 use std::io::{Error, ErrorKind};
 
-const BOARD_SIZE: usize = 10;
+const BOARD_SIZE: usize = 1000;
 
 struct Coord {
-  pub x: usize,
-  pub y: usize,
+  pub x: u32,
+  pub y: u32,
 }
 
 pub struct Board {
@@ -24,13 +24,6 @@ impl Board {
     return board;
   }
 
-  // pub fn new() -> Board {
-  //   Board {
-  //     name: "Board",
-  //     layout: [[0; BOARD_SIZE]; BOARD_SIZE],
-  //   }
-  // }
-
   pub fn print(&self) {
     println!("{}", self.name);
     for y in self.layout {
@@ -41,7 +34,7 @@ impl Board {
           print!("{}", x);
         }
         if i != y.len() {
-          print!("\t");
+          print!(" ");
         }
       }
       print!("\n")
@@ -49,76 +42,70 @@ impl Board {
     print!("\n");
   }
 
-  // pub fn call(&mut self, val: u32) {
-  //   'outer: for (i, row) in self.layout.iter().enumerate() {
-  //     for (j, tile) in row.iter().enumerate() {
-  //       if tile.value == val {
-  //         self.layout[i][j].is_marked = true;
-  //         break 'outer;
-  //       }
-  //     }
-  //   }
-  // }
-
-  // pub fn call_and_validate_win(&mut self, val: u32) -> bool {
-  //   self.call(val);
-
-  //   return self.check_win();
-  // }
-
-  // pub fn add_unmarked(&self) -> u32 {
-  //   let mut num: u32 = 0;
-
-  //   for row in self.layout {
-  //     for tile in row {
-  //         if !tile.is_marked {
-  //           num += tile.value;
-  //         }
-  //     }
-  //   }
-
-  //   return num;
-  // }
   pub fn add_point(&mut self, x: usize, y: usize) {
-    self.layout[x][y] += 1;
+    self.layout[y][x] += 1;
   }
 
   pub fn read_vents_from_file(&mut self, file_name: &str) {
     let file = File::open(file_name).unwrap();
     let reader = BufReader::new(file);
 
-    for (i, line) in reader.lines().enumerate() {
+    for line in reader.lines() {
       let line = line.unwrap();
 
-      let (mut coord1, coord2) = read_coord_from_string(line).unwrap();
+      let (coord1, coord2) = read_coord_from_string(line).unwrap();
 
-      let x_dir: i32 = if (coord1.x - coord2.x) < 0 { 1 } else if coord1.x - coord2.x > 0 { -1 } else { 0 };
-      let y_dir: i32 = if coord1.y - coord2.y < 0 { 1 } else if coord1.y - coord2.y > 0 { -1 } else { 0 };
+      if coord1.x == coord2.x {
+        // println!("{},{} -> {},{}", coord1.x, coord1.y, coord2.x, coord2.y);
+        let diff = coord1.y as i32 - coord2.y as i32;
 
-      while coord1.x != coord2.x && coord1.y != coord2.y {
-        self.add_point(coord1.x, coord1.y);
+        if diff < 0 {
+          for i in 0..(diff.abs() + 1) {
+            // println!("dy: {}", coord1.y as i32 + i);
+            self.add_point(coord1.x as usize, (coord1.y as i32 + i) as usize);
+          }
+        } else {
+          for i in 0..(diff + 1) {
+            // println!("dy: {}", coord1.y as i32 - i);
+            self.add_point(coord1.x as usize, (coord1.y as i32 - i) as usize);
+          }
+        }
+        self.print();
+      } else if coord1.y == coord2.y {
+        // println!("{},{} -> {},{}", coord1.x, coord1.y, coord2.x, coord2.y);
+        let diff = coord1.x as i32 - coord2.x as i32;
 
-        if coord1.x != coord2.x {
-          coord1.x = add_dir(coord1.x, x_dir);
+        if diff < 0 {
+          for i in 0..(diff.abs() + 1) {
+            // println!("dx: {}\t{}", coord1.y as i32 + i, diff);
+            self.add_point((coord1.x as i32 + i) as usize, coord1.y as usize);
+          }
+        } else {
+          for i in 0..(diff + 1) {
+            // println!("dx: {}\t{}", coord1.x as i32 - i, diff);
+            self.add_point((coord1.x as i32 - i) as usize, coord1.y as usize);
+          }
         }
 
-        if coord1.y != coord2.y {
-          coord1.y = add_dir(coord1.y, y_dir);
-        }
+        self.print();
+      } else {
+        println!("Throwing out {},{} -> {},{}", coord1.x, coord1.y, coord2.x, coord2.y);
       }
-
-      self.add_point(0, 0);
     }
   }
-}
 
-fn add_dir(val: usize, dir: i32) -> usize {
-  if dir < 0 {
-    val - 1
-  } else if dir > 0 {
-    val + 1
-  } else {
-    val
+  pub fn add_twos_or_more(&self) -> i32 {
+    let mut total: i32 = 0;
+
+    for y in self.layout {
+      for x in y.iter() {
+        if *x >= 2 {
+          total += 1;
+        } 
+      }
+    }
+
+    return total;
   }
 }
 
@@ -132,20 +119,20 @@ fn read_coord_from_string(line: String) -> Result<(Coord, Coord)> {
     ));
   }
 
-  let pos1_str: Vec<usize> = vent_instructions[0]
+  let pos1_str: Vec<u32> = vent_instructions[0]
     .split(",")
     .into_iter()
-    .map(|x| x.parse::<usize>().unwrap())
+    .map(|x| x.parse::<u32>().unwrap())
     .collect();
   let pos1: Coord = Coord {
     x: pos1_str[0],
     y: pos1_str[1],
   };
 
-  let pos2_str: Vec<usize> = vent_instructions[2]
+  let pos2_str: Vec<u32> = vent_instructions[2]
     .split(",")
     .into_iter()
-    .map(|x| x.parse::<usize>().unwrap())
+    .map(|x| x.parse::<u32>().unwrap())
     .collect();
 
   let pos2: Coord = Coord {
--- 2021/05/src/main.rs
+++ 2021/05/src/main.rs
@@ -2,14 +2,18 @@ mod board;
 
 use board::{Board};
 
-const FILE_PATH: &str = "resources/exampleInput.txt";
+const FILE_PATH: &str = "resources/input.txt";
 
 fn part1() {
     println!("File: {}", FILE_PATH);
     let mut board = Board::new("Board");
     board.read_vents_from_file(FILE_PATH);
 
-    board.print();
+    // board.print();
+
+    let total = board.add_twos_or_more();
+
+    println!("Total: {}", total);
 }
 
 fn main() {
