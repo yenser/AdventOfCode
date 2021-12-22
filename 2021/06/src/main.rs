44482164760e25fba05c3c3d5f23ae82cad6236d
Jens Streck <jens.streck@gmail.com>
2021-12-21 22:27:49 -0600

    day 6 wip

--- 2021/06/src/main.rs
+++ 2021/06/src/main.rs
@@ -1,3 +1,54 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const FILE_PATH: &str = "resources/input.txt";
+
+fn simulate_fish(days: usize) {
+    println!("File: {}", FILE_PATH);
+
+    let file = File::open(FILE_PATH).unwrap();
+    let reader = BufReader::new(file);
+    let list = reader.lines().next().unwrap();
+    
+    let mut lantern_fish: Vec<u8> = list.unwrap().split(',').map(|x| x.parse::<u8>().unwrap()).collect();
+
+    let mut fish_to_add = 0;
+    for i in 0..days {
+        lantern_fish.iter_mut().for_each(|x| {
+            if *x == 0 {
+                fish_to_add += 1;
+                *x = 6;
+            } else {
+                *x -= 1
+            }
+        });
+
+        let mut new_fish: Vec<u8> = vec![8; fish_to_add];
+
+        lantern_fish.append(&mut new_fish);
+        // for _ in 0..fish_to_add {
+        //     lantern_fish.push(8);
+        // }
+
+        fish_to_add = 0;
+        println!("After {} day: {} total fish", i + 1, lantern_fish.len());
+    }
+    // println!("Total: {}", total);
+}
+
+// fn part2() {
+//     println!("File: {}", FILE_PATH);
+//     let mut board = Board::new("Board");
+//     board.read_vents_from_file(FILE_PATH);
+
+//     // board.print();
+
+//     let total = board.add_twos_or_more();
+
+//     println!("Total: {}", total);
+// }
+
 fn main() {
-    println!("Hello, world!");
+    // simulate_fish(80);
+    simulate_fish(256);
 }
